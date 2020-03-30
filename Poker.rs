f725f348d7fe046f90c61310e9a5ccb87410a1c3
Thomas <thomasyoungaudet@gmail.com>
2020-03-30 10:45:48 -0400

    Now I want to make the same program but in Rust

---
+++ Poker.rs
@@ -0,0 +1,12 @@
+//let perm:[u32;10] = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+//let winner:[&str;5] = deal(perm);
+//Your deal function should accept an array of 10 unsigned integers, and your deal function
+//should return an array of five string slices. The strings should be formatted in the same way
+//as all previous assignments.
+//Your submission should not include a main() function. Our tester will implement main()
+
+
+fn deal() {
+
+	
+}
